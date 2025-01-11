7c5b8d487931008a8d52d61c1a99a16ddc1f3439
Gideao Frota <gideao.frota@blip.ai>
2025-01-10 21:14:10 -0300

    feat: add cleark for authentication

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,19 @@
-export default function Home() {
-  return <h1 className="text-red-500">hello</h1>;
-}
+import { UserButton } from "@clerk/nextjs";
+import { auth } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
+
+const Home = async () => {
+  const { userId } = await auth();
+
+  if (!userId) {
+    redirect("/login");
+  }
+
+  return (
+    <div className="flex h-full items-center justify-center">
+      <UserButton showName />
+    </div>
+  );
+};
+
+export default Home;

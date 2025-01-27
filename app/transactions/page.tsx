835876d0ba1182f20036bff8eec4fa4dc3db4247
Gideao Frota <gideao.frota@blip.ai>
2025-01-27 09:41:38 -0300

    chore: created connetion to local db

--- app/transactions/page.tsx
+++ app/transactions/page.tsx
@@ -1,5 +1,15 @@
-const Transactions = () => {
-  return (  );
-}
- 
+import { db } from "../_lib/prisma";
+
+const Transactions = async () => {
+  const transactions = await db.transaction.findMany({});
+
+  return (
+    <div>
+      {transactions.map((transaction) => (
+        <div key={transaction.id}>{transaction.name}</div>
+      ))}
+    </div>
+  );
+};
+
 export default Transactions;

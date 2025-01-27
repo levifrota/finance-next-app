20d7dd207d3387c98a4e8ec61f9ed02beb520e5c
Gideao Frota <gideao.frota@blip.ai>
2025-01-27 14:17:48 -0300

    feat: add transaction screen

--- app/transactions/page.tsx
+++ app/transactions/page.tsx
@@ -1,13 +1,23 @@
+import { ArrowDownUpIcon } from "lucide-react";
 import { db } from "../_lib/prisma";
+import { Button } from "../components/ui/button";
+import { DataTable } from "../components/ui/data-table";
+import { transactionColumns } from "./_columns";
 
 const Transactions = async () => {
   const transactions = await db.transaction.findMany({});
 
   return (
-    <div>
-      {transactions.map((transaction) => (
-        <div key={transaction.id}>{transaction.name}</div>
-      ))}
+    <div className="space-y-6 p-6">
+      <div className="flex w-full items-center justify-between">
+        <h1 className="text-2xl font-bold">Transações</h1>
+        <Button className="rounded-full">
+          <ArrowDownUpIcon />
+          Adicionar Transação
+        </Button>
+      </div>
+
+      <DataTable columns={transactionColumns} data={transactions} />
     </div>
   );
 };

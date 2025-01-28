c4e15216598b1a950ddbb6bc225d5f60ba47dc9a
Gideao Frota <gideao.frota@blip.ai>
2025-01-27 21:46:35 -0300

    feat: create add transaction dialog

--- app/_components/add-transaction-button.tsx
+++ app/_components/add-transaction-button.tsx
@@ -1,3 +1,6 @@
+"use client";
+
+import { z } from "zod";
 import { ArrowDownUpIcon } from "lucide-react";
 import { Button } from "./ui/button";
 import {
@@ -9,8 +12,60 @@ import {
   DialogDescription,
   DialogFooter,
 } from "./ui/dialog";
+import {
+  TransactionCategory,
+  TransactionPaymentMethod,
+  TransactionType,
+} from "@prisma/client";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "./ui/form";
+import { Input } from "./ui/input";
+import { MoneyInput } from "./money-input";
+
+const formSchema = z.object({
+  name: z.string().min(1, {
+    message: "O nome é obrigatório.",
+  }),
+  amount: z.string().trim().min(1, {
+    message: "O valor é obrigatório.",
+  }),
+  type: z.nativeEnum(TransactionType, {
+    required_error: "O tipo é obrigatório.",
+  }),
+  category: z.nativeEnum(TransactionCategory, {
+    required_error: "A categoria é obrigatória.",
+  }),
+  paymentMethod: z.nativeEnum(TransactionPaymentMethod, {
+    required_error: "O método de pagamento é obrigatório.",
+  }),
+  date: z.date({
+    required_error: "A data é obrigatória.",
+  }),
+});
 
 const AddTransactionButton = () => {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      name: "",
+      amount: "",
+      type: TransactionType.EXPENSE,
+      category: TransactionCategory.OTHER,
+      paymentMethod: TransactionPaymentMethod.CASH,
+      date: new Date(),
+    },
+  });
+
+  const onSubmit = () => {};
+
   return (
     <Dialog>
       <DialogTrigger asChild>
@@ -24,10 +79,43 @@ const AddTransactionButton = () => {
           <DialogTitle>Adicionar Transação</DialogTitle>
           <DialogDescription>Insira as informações abaixo</DialogDescription>
         </DialogHeader>
-        <DialogFooter>
-          <Button variant="outline">Cancelar</Button>
-          <Button>Adicionar</Button>
-        </DialogFooter>
+
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+            <FormField
+              control={form.control}
+              name="name"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Nome</FormLabel>
+                  <FormControl>
+                    <Input placeholder="Digite o nome" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+
+            <FormField
+              control={form.control}
+              name="amount"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Valor</FormLabel>
+                  <FormControl>
+                    <MoneyInput placeholder="Digite o valor" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+
+            <DialogFooter>
+              <Button variant="outline">Cancelar</Button>
+              <Button>Adicionar</Button>
+            </DialogFooter>
+          </form>
+        </Form>
       </DialogContent>
     </Dialog>
   );

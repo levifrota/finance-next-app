e9e4d400d62c4ea73bd2d4ae0becfbb37c86677a
Gideao Frota <gideao.frota@blip.ai>
2025-01-29 00:35:45 -0300

    feat: add last transactions components

--- app/_constants/transactions.ts
+++ app/_constants/transactions.ts
@@ -16,6 +16,16 @@ export const TRANSACTION_CATEGORY_LABELS = {
   SALARY: "Salário",
 };
 
+export const TRANSACTION_PAYMENT_METHOD_ICONS = {
+  [TransactionPaymentMethod.CREDIT_CARD]: "credit-card.svg",
+  [TransactionPaymentMethod.DEBIT_CARD]: "debit-card.svg",
+  [TransactionPaymentMethod.BANK_TRANSFER]: "bank-transfer.svg",
+  [TransactionPaymentMethod.BANK_SLIP]: "bank-slip.svg",
+  [TransactionPaymentMethod.CASH]: "money.svg",
+  [TransactionPaymentMethod.PIX]: "pix.svg",
+  [TransactionPaymentMethod.OTHER]: "other.svg",
+};
+
 export const TRANSACTION_PAYMENT_METHOD_LABELS = {
   CREDIT_CARD: "Cartão de crédito",
   DEBIT_CARD: "Cartão de débito",
--- app/_data/get-dashboard/index.ts
+++ app/_data/get-dashboard/index.ts
@@ -77,6 +77,12 @@ export const getDashboard = async (month: string) => {
     ),
   }));
 
+  const lastTransactions = await db.transaction.findMany({
+    where,
+    orderBy: { date: "desc" },
+    take: 10,
+  });
+
   return {
     depositsTotal,
     investmentsTotal,
@@ -84,5 +90,6 @@ export const getDashboard = async (month: string) => {
     balance,
     typesPercentage,
     totalExpensePerCategory,
+    lastTransactions,
   };
 };

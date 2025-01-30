3e83d5ea889ddacc03ddf14af4d6a52c4d1a5b62
Gideao Frota <gideao.frota@blip.ai>
2025-01-29 23:56:18 -0300

    fix: try to change ai model

--- app/(home)/_actions/generate-ai-report/index.ts
+++ app/(home)/_actions/generate-ai-report/index.ts
@@ -2,8 +2,9 @@
 
 import { db } from "@/app/_lib/prisma";
 import { auth, clerkClient } from "@clerk/nextjs/server";
-import OpenAI from "openai";
+// import OpenAI from "openai";
 import { GenerateAiReportSchema, generateAiReportSchema } from "./schema";
+import { GoogleGenerativeAI } from "@google/generative-ai";
 
 export const generateAiReport = async ({ month }: GenerateAiReportSchema) => {
   generateAiReportSchema.parse({ month });
@@ -23,10 +24,12 @@ export const generateAiReport = async ({ month }: GenerateAiReportSchema) => {
     );
   }
 
-  const openAi = new OpenAI({
-    apiKey: process.env.OPENAI_API_KEY,
-    baseURL: "https://models.inference.ai.azure.com",
-  });
+  const genAI = new GoogleGenerativeAI(process.env.OPENAI_API_KEY as string);
+  const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
+  // const openAi = new OpenAI({
+  //   apiKey: process.env.OPENAI_API_KEY,
+  //   baseURL: "https://api.aimlapi.com/v1",
+  // });
   const transactions = await db.transaction.findMany({
     where: {
       date: {
@@ -35,28 +38,40 @@ export const generateAiReport = async ({ month }: GenerateAiReportSchema) => {
       },
     },
   });
-  const content = `Gere um relatório com insights sobre as minhas finanças, com dicas e orientações de como melhorar minha vida financeira. As transações estão divididas por ponto e vírgula. A estrutura de cada uma é {DATA}-{TIPO}-{VALOR}-{CATEGORIA}. São elas:
+  const prompt = `Você é um especialista em gestão e organização de finanças pessoais. Você ajuda as pessoas a organizarem melhor as suas finanças. Gere um relatório com insights sobre as minhas finanças, com dicas e orientações de como melhorar minha vida financeira. As transações estão divididas por ponto e vírgula. A estrutura de cada uma é {DATA}-{TIPO}-{VALOR}-{CATEGORIA}. São elas:
   ${transactions
     .map(
       (transaction) =>
         `${transaction.date.toLocaleDateString("pt-BR")}-R$${transaction.amount}-${transaction.type}-${transaction.category}`,
     )
-    .join(";")}`;
+    .join(";")}. Não há necessidade de mostrar cada transação.`;
+  console.log(prompt);
+  const result = await model.generateContent(prompt);
+  // const content = `Gere um relatório com insights sobre as minhas finanças, com dicas e orientações de como melhorar minha vida financeira. As transações estão divididas por ponto e vírgula. A estrutura de cada uma é {DATA}-{TIPO}-{VALOR}-{CATEGORIA}. São elas:
+  // ${transactions
+  //   .map(
+  //     (transaction) =>
+  //       `${transaction.date.toLocaleDateString("pt-BR")}-R$${transaction.amount}-${transaction.type}-${transaction.category}`,
+  //   )
+  //   .join(";")}`;
 
-  const completion = await openAi.chat.completions.create({
-    model: "gpt-4o-mini",
-    messages: [
-      {
-        role: "system",
-        content:
-          "Você é um especialista em gestão e organização de finanças pessoais. Você ajuda as pessoas a organizarem melhor as suas finanças.",
-      },
-      {
-        role: "user",
-        content,
-      },
-    ],
-  });
+  // console.log("content", content);
+  // debugger;
 
-  return completion.choices[0].message.content;
+  // const completion = await openAi.chat.completions.create({
+  //   model: "gemini-1.5-flash",
+  //   messages: [
+  //     {
+  //       role: "system",
+  //       content:
+  //         "Você é um especialista em gestão e organização de finanças pessoais. Você ajuda as pessoas a organizarem melhor as suas finanças.",
+  //     },
+  //     {
+  //       role: "user",
+  //       content,
+  //     },
+  //   ],
+  // });
+
+  return result.response.text();
 };

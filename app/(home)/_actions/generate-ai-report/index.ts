3acf7355526b5ea1ef4a7b38be9c0c8465882fba
Gideao Frota <gideao.frota@blip.ai>
2025-01-30 00:12:04 -0300

    fix: back to openai api

--- app/(home)/_actions/generate-ai-report/index.ts
+++ app/(home)/_actions/generate-ai-report/index.ts
@@ -2,8 +2,8 @@
 
 import { db } from "@/app/_lib/prisma";
 import { auth, clerkClient } from "@clerk/nextjs/server";
+import OpenAI from "openai";
 import { GenerateAiReportSchema, generateAiReportSchema } from "./schema";
-import { GoogleGenerativeAI } from "@google/generative-ai";
 
 export const generateAiReport = async ({ month }: GenerateAiReportSchema) => {
   generateAiReportSchema.parse({ month });
@@ -23,8 +23,9 @@ export const generateAiReport = async ({ month }: GenerateAiReportSchema) => {
     );
   }
 
-  const genAI = new GoogleGenerativeAI(process.env.OPENAI_API_KEY as string);
-  const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
+  const openAi = new OpenAI({
+    apiKey: process.env.OPENAI_API_KEY,
+  });
   const transactions = await db.transaction.findMany({
     where: {
       date: {
@@ -33,15 +34,28 @@ export const generateAiReport = async ({ month }: GenerateAiReportSchema) => {
       },
     },
   });
-  const prompt = `Você é um especialista em gestão e organização de finanças pessoais. Você ajuda as pessoas a organizarem melhor as suas finanças. Gere um relatório com insights sobre as minhas finanças, com dicas e orientações de como melhorar minha vida financeira. As transações estão divididas por ponto e vírgula. A estrutura de cada uma é {DATA}-{TIPO}-{VALOR}-{CATEGORIA}. São elas:
+  const content = `Gere um relatório com insights sobre as minhas finanças, com dicas e orientações de como melhorar minha vida financeira. As transações estão divididas por ponto e vírgula. A estrutura de cada uma é {DATA}-{TIPO}-{VALOR}-{CATEGORIA}. São elas:
   ${transactions
     .map(
       (transaction) =>
         `${transaction.date.toLocaleDateString("pt-BR")}-R$${transaction.amount}-${transaction.type}-${transaction.category}`,
     )
-    .join(";")}. Não há necessidade de mostrar cada transação.`;
-  console.log(prompt);
-  const result = await model.generateContent(prompt);
+    .join(";")}`;
 
-  return result.response.text();
+  const completion = await openAi.chat.completions.create({
+    model: "gpt-4o-mini",
+    messages: [
+      {
+        role: "system",
+        content:
+          "Você é um especialista em gestão e organização de finanças pessoais. Você ajuda as pessoas a organizarem melhor as suas finanças.",
+      },
+      {
+        role: "user",
+        content,
+      },
+    ],
+  });
+
+  return completion.choices[0].message.content;
 };

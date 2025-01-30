f87cc7fa6a095838b2508c2ed6e4429e9afe7627
Gideao Frota <gideao.frota@blip.ai>
2025-01-29 22:35:41 -0300

    fix: improve responsivity and adjust route after subscription

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -13,18 +13,18 @@ const LoginPage = async () => {
   }
 
   return (
-    <div className="grid h-full grid-cols-2">
+    <div className="h-full sm:grid sm:grid-cols-2">
       <div className="flex h-full max-w-[550px] flex-col justify-center p-8">
         <Image
           src="/logo.svg"
           width={173}
           height={39}
-          alt="Finance AI"
+          alt="Poupa aí"
           className="mb-8"
         />
-        <h1 className="mb-3 text-4xl font-bold">Bem Vindo</h1>
+        <h1 className="mb-3 text-4xl font-bold">Bem Vindo(a)!</h1>
         <p className="mb-8 text-muted-foreground">
-          A Finance AI é uma plataforma de gestão financeira que utiliza IA para
+          A Poupa.ai é uma plataforma de gestão financeira que utiliza IA para
           monitorar suas movimentações, e oferecer insights personalizados,
           facilitando o controle do seu orçamento.
         </p>

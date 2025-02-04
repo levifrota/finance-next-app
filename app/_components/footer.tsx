ab5fd04d9e05f70193872f107d8a51932bcaca9e
salmomascarenhas <salmo.cruz@gmail.com>
2025-02-04 10:57:46 -0300

    fix: change Menubar positioning from absolute to relative in footer component

--- app/_components/footer.tsx
+++ app/_components/footer.tsx
@@ -25,7 +25,7 @@ const Footer = () => {
   }
 
   return (
-    <Menubar className="absolute bottom-0 flex h-16 w-full justify-around">
+    <Menubar className="relative bottom-0 flex h-16 w-full justify-around">
       <MenubarMenu>
         <MenubarTrigger>
           <Link href="/" className={linkClasses("/")}>

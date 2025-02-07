033bd762dd7c635c1030d63fcfa4634fceef7147
Gideao Frota <gideao.frota@blip.ai>
2025-02-07 14:25:32 -0300

    fix: attempt to correct login behaviour

--- app/_lib/firebaseAdmin.ts
+++ app/_lib/firebaseAdmin.ts
@@ -1,4 +1,3 @@
-// firebaseAdmin.ts
 import admin from "firebase-admin";
 // import serviceAccount from "../../serviceAccountKey.json" assert { type: "json" };
 
@@ -7,7 +6,7 @@ if (!admin.apps.length) {
     credential: admin.credential.cert({
       projectId: process.env.FIREBASE_ADMIN_PROJECT_ID,
       clientEmail: process.env.FIREBASE_ADMIN_CLIENT_EMAIL,
-      privateKey: process.env.FIREBASE_ADMIN_PRIVATE_KEY?.replace(/\\n/g, "\n"),
+      privateKey: process.env.FIREBASE_ADMIN_PRIVATE_KEY,
     }),
   });
 }
